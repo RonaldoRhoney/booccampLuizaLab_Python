b5b89933f76fd3d3b15c690bbac38f3de086745b
Ronaldo Martins <ronaldorhoney@hotmail.com>
2026-04-17 23:06:32 -0300

    Atualizando Registro da tabela

--- banco de dados/conexao.py
+++ banco de dados/conexao.py
@@ -2,22 +2,32 @@ import sqlite3
 from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
-
 conexao = sqlite3.connect(ROOT_PATH / 'meu_banco.sqlite3')
-
 cursor = conexao.cursor()
 
-cursor.execute("""
-CREATE TABLE clientes (
-    id INTER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT,
-    email TEXT
-)
-""")
 
-conexao.commit()
-conexao.close()
+def criar_tabela(conexao, cursor):
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS clientes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome TEXT,
+        email TEXT
+    )
+    """)
+    conexao.commit()
 
 
-data = ("Rhoney", "ronaldorhoney@hotmail.com")
-cursor.execute("INSERT INTO clientes (nome, email) VALUES (?, ?)", data)
+def inserir_cliente(conexao, cursor, nome, email):
+    data = (nome, email)
+    cursor.execute(
+        "INSERT INTO clientes (nome, email) VALUES (?, ?)", data
+    )
+    conexao.commit()
+
+
+def atualizar_registro(conexao, cursor, id, nome, email):
+    data = (nome, email, id)
+    cursor.execute(
+        "UPDATE clientes SET nome = ?, email = ? WHERE id = ?", data
+    )
+    conexao.commit()
