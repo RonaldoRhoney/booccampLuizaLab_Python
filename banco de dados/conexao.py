686717190e7f61dae79f6316712c0da2c26b0412
Ronaldo Martins <ronaldorhoney@hotmail.com>
2026-04-17 22:05:27 -0300

    criação de banco de dados com tabelas

--- banco de dados/conexao.py
+++ banco de dados/conexao.py
@@ -3,5 +3,17 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-conexao = sqlite3.connect(ROOT_PATH / 'meu_banco.db')
-print(conexao)
+conexao = sqlite3.connect(ROOT_PATH / 'meu_banco.sqlite3')
+
+cursor = conexao.cursor()
+
+cursor.execute("""
+CREATE TABLE clientes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT,
+    email TEXT
+)
+""")
+
+conexao.commit()
+conexao.close()
